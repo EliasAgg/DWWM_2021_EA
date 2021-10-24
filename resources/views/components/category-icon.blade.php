41c1779b7630851ac4235edf9c2bb44cf80cd93d
Elias <elias.aggoune@elias-agg.com>
2021-10-24 19:14:58 +0200

    stylized category icons

--- resources/views/components/category-icon.blade.php
+++ resources/views/components/category-icon.blade.php
@@ -1,14 +1,14 @@
 @switch($id)
     @case(1)
-        <i class="fas fa-recycle"></i>
+        <i class="fas fa-recycle" style="color: rgb(153, 179, 7);"></i>
     @break
     @case(2)
-        <i class="fal fa-wrench"></i>
+        <i class="fas fa-wrench" style="color: rgb(88, 88, 88);"></i>
     @break
     @case(3)
-        <i class="far fa-store"></i>
+        <i class="fas fa-store" style="color: rgb(233, 96, 17);"></i>
     @break
     @case(4)
-        <i class="fas fa-box-full"></i>
+        <i class="fas fa-box-full" style="color: rgb(17, 165, 233);"></i>
     @break
 @endswitch
