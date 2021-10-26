7d6d08b7529b6fe662f534269ccc0202b03042aa
Elias <elias.aggoune@elias-agg.com>
2021-10-26 18:09:53 +0200

    modified single view

--- resources/views/business/single.blade.php
+++ resources/views/business/single.blade.php
@@ -15,7 +15,7 @@
                 <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
             </div>
         </section>
-        <section class="relative py-16 bg-blueGray-200">
+        <section class="relative py-16">
             <div class="container mx-auto px-4">
                 <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
                     <div class="px-6">
@@ -42,26 +42,38 @@
                         </div>
                         <div class="text-center mt-12">
                             <h3 class="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">
-                                {{$business->name}}
+                                {{ $business->name }}
                             </h3>
                             <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                                 <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-                                {{$business->address}}
+                                {{ $business->address }}
+                            </div>
+                            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                                <i class="fas fa-phone mr-2 text-lg text-blueGray-400"></i>
+                                {{ formatPhone($business->phone) }}
+                            </div>
+                            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                                <i class="fas fa-envelope mr-2 text-lg text-blueGray-400"></i>
+                                {{ $business->email }}
+                            </div>
+                            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                                <i class="fas fa-link mr-2 text-lg text-blueGray-400"></i>
+                                <a href="https://{{ $business->link }}">{{ $business->link }}</a>
                             </div>
                             <div class="mb-2 text-blueGray-600 mt-10">
-                                <x-category-icon :id="$business->main_category_id"/>{{$business->MainCategory->name}}
+                                <x-category-icon :id="$business->main_category_id" />{{ $business->MainCategory->name }}
                             </div>
                             <div class="mb-2 text-blueGray-600">
-                                <x-sub-category-icon :id="$business->sub_category_id"/>{{$business->SubCategory->name}}
+                                <x-sub-category-icon :id="$business->sub_category_id" />
+                                {{ $business->SubCategory->name }}
                             </div>
                         </div>
                         <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
                             <div class="flex flex-wrap justify-center">
                                 <div class="w-full lg:w-9/12 px-4">
                                     <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-                                        {{$business->description}}
+                                        {{ $business->description }}
                                     </p>
-                                    <a href="#pablo" class="font-normal text-pink-500">Show more</a>
                                 </div>
                             </div>
                         </div>
