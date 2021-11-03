8e78c03ebf498741e1e13f035c6fdc492265145d
Elias <elias.aggoune@elias-agg.com>
2021-11-03 15:58:02 +0100

    microadjustments

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,7 +28,8 @@
                         <button
                             class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>
-                                <i class="fas fa-{{Auth::user()->is_admin ? 'user-tie' : 'user'}} mr-3 text-2xl self-center text-gray-400"></i>
+                                <i
+                                    class="fas fa-{{ Auth::user()->is_admin ? 'user-tie' : 'user' }} mr-3 text-2xl self-center text-gray-400"></i>
                             </div>
                             <div> {{ Auth::user()->name }}</div>
 
@@ -83,9 +84,9 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
             @if (Auth::user()->is_admin)
-            <x-responsive-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
-                {{ __('Administration') }}
-            </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
+                    {{ __('Administration') }}
+                </x-responsive-nav-link>
             @endif
         </div>
 
